1d55b39b2436ae7b79c1bbefa7c34317a5062a12
Diego Hernandez <diegoalejandro061292@gmail.com>
2023-03-22 18:28:15 -0500

    cambios

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+// const request = require('request');
+// // request('https://pokeapi.co/api/v2/pokemon?limit=20', function(error, response, body) {
+
+//   request('https://pokeapi.co/api/v2/pokemon', function(error, response, body) {
+
+//   if (response.statusCode === 200) {
+//     const bodyJSON = JSON.parse(body);
+//     console.log(bodyJSON);
+//   } else {
+//     console.log(error);
+//   }
+// }); 
 const request = require('request');
-request('https://pokeapi.co/api/v2/pokemon?limit=20', function(error, response, body) {
-  if (response.statusCode === 200) {
-    const bodyJSON = JSON.parse(body);
-    console.log(bodyJSON);
-  } else {
-    console.log(error);
-  }
-}); 
+function peticionPokemon(name, type, url){
+request.get(`https://pokeapi.co/api/v2/pokemon?${name}${type}${url}`,function(error, response, body){
+if (response.statusCode === 200){
+  const bodyJSON = JSON.parse(body);
+  console.log(bodyJSON);
+
+}
+
+})
+
+
+}
