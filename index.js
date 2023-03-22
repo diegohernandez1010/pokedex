cee99260b64b19343a28c320ef6201cdc6ff70f5
Diego Hernandez <diegoalejandro061292@gmail.com>
2023-03-21 21:59:24 -0500

    inicio

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const request = require ('request');
-request.get("https://pokeapi.co/api/v2/berry-firmness/{id or name}/")
+const request = require('request');
+request.get('https://pokeapi.co/api/v2/pokemon'), function(error, response, body){
+
+if(response.statusCode === 200){
+const bodyJson = JSON.parse(body);
+console.log(bodyJson);
+
+}
+}
+console.log(response.statusCode);
